4b731255d49e298d39d5926f1a9ddb0d55b77a5c
Mark Sanchez <dlsu.sachii@gmail.com>
2017-07-05 23:28:29 +0800

    Added a few more comments

--- BucketSort/src/bucketsort/BucketSort.java
+++ BucketSort/src/bucketsort/BucketSort.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * COMALGO S21
+ * Authors
+ * Mark Christian Sanchez
+ * Miguel Jason Cruz
  */
 package bucketsort;
 
@@ -140,6 +141,7 @@ public class BucketSort {
         return list;
     }
     
+    // Partially complete bucketsort
     public static Integer[] recursiveBucketSort(Integer[] list, int bit) {
         
         boolean popable = false;
